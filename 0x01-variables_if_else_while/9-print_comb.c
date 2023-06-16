188d305f5a5c78595ae928c3ceff7249a61051d1
bawek <bawekemekonnenasres884@gmail.com>
2023-06-17 01:36:15 +0900

    xcvbnm,

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,21 @@
  */
 int main(void)
 {
-	int num;
+        int num;
 
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
-	}
+        for (num = 0; num <= 9; num++)
+        {
+                putchar((num % 10) + '0');
+                if (num == 9)
+                        continue;
 
-	putchar('\n');
+                putchar(',');
+                putchar(' ');
+        }
 
-	return (0);
+        putchar('\n');
+
+        return (0);
 }
+[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Ccontinue;[B[B[B[B[B[B[B[B[B[B[B[B
+chmod u+x [A
