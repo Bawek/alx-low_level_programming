a0cb8b68b99391d6a9db6edbe8ee94679634c898
bawek <bawekemekonnenasres884@gmail.com>
2023-06-17 02:51:18 +0900

    cvbnm,

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,22 @@
  */
 int main(void)
 {
-	int num;
+        int num;
 
-	for (num = 0; num <= 9; num++)
-	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-		
-		putchar(',');
-		putchar(' ');
-	}
+        for (num = 0; num <= 9; num++)
+        {
+                putchar((num % 10) + '0');
+                if (num == 9)
+                        continue;
 
-	putchar('\n');
+                putchar(',');
+                putchar(' ');
+        }
 
-	return (0);
+        putchar('\n');
+
+        return (0);
 }
+[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[  putchar(',');
+                putchar(' ');
+[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[D[D[D[D[Dpu[B[C[C[C[C[C[C[C[C[C[C[C[A[D[D[D[D[D[D[D[D[D[D[B[B[B[B[B[B[B[B[B
