06bf436bc762c972fc9c68f5508e9e400c73d709
bawek <bawekemekonnenasres884@gmail.com>
2023-06-17 03:05:28 +0900

    kezih behuala almokrim

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-        int num;
+	int num;
 
-        for (num = 0; num <= 9; num++)
-        {
-                putchar((num % 10) + '0');
-                if (num == 9)
-                   continue;
-                   putchar(',');
-                   putchar(' ');
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
 
-        }
+	putchar('\n');
 
-        putchar('\n');
-
-        return (0);
-}
+	return (0);
+}[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[C[C[C*[B[D*except ZERO [A[D[D[D[D[D[D[D[D[D[D[Dgo[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B
