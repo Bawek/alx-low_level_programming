19231eaaa8717a13d63494031a71febe5e8bf284
bawek <bawekemekonnenasres884@gmail.com>
2023-06-17 02:40:51 +0900

    cvbnm,

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,19 @@
-[A[B[B[A[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[A[A[A[A[A[A[A[A[A[A[A[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[A[A[B[A[A[A[A[A[A[A[A[
+#include <studio.h>
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
