d8fb55bcb0859d9c2de54b2e3d7012238e23e295
Bawek <bawekemekonnenasres@gmail.com>
2023-06-21 13:27:02 -0700

    fdfgnmn,

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*
+ * this print alphabet ten times
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/*good
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		return (-i);
+	return (i);
+}
+
