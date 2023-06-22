6d283ec2e6134e8a25b49cfbc3c89135e2043013
Bawek <bawekemekonnenasres@gmail.com>
2023-06-22 15:44:58 -0700

    dsfgn

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line n times.
+ *
+ * ok
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
+
+
